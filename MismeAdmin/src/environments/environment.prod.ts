d9ef47c0e661c53a25b81eedf2d5f960009631e2
Yoandry <mazinchess@gmail.com>
2020-04-26 19:35:31 -0400

    Update environment.

--- MismeAdmin/src/environments/environment.prod.ts
+++ MismeAdmin/src/environments/environment.prod.ts
@@ -5,6 +5,8 @@
  */
 export const environment = {
   production: true,
-  serverUrl: 'http://ec2-52-15-173-73.us-east-2.compute.amazonaws.com/api/',
-  hubUrl: 'http://ec2-52-15-173-73.us-east-2.compute.amazonaws.com/'
+  // serverUrl: 'http://ec2-52-15-173-73.us-east-2.compute.amazonaws.com/api/',
+  serverUrl: 'http://ec2-34-244-181-197.eu-west-1.compute.amazonaws.com/api/',
+  // hubUrl: 'http://ec2-52-15-173-73.us-east-2.compute.amazonaws.com/'
+  hubUrl: 'http://ec2-34-244-181-197.eu-west-1.compute.amazonaws.com/'
 };
