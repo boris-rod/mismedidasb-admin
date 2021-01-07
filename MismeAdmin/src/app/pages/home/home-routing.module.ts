7623d5c18fe505f143b7c76db16e62ae54211712
Yoandry <mazinchess@gmail.com>
2021-01-07 15:57:49 -0500

    Add users menu.

--- MismeAdmin/src/app/pages/home/home-routing.module.ts
+++ MismeAdmin/src/app/pages/home/home-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthenticationGuard } from 'src/app/core-mismes';
 import { HomeComponent } from './home.component';
+import { UsersModule } from '../users/users.module';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthenticationGuard] },
-  // { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  {
+    path: '', component: HomeComponent, canActivate: [AuthenticationGuard], children: [
+      { path: 'users', loadChildren: () => import('../users/users.module').then(m => m.UsersModule) }
+    ]
+  },
+
 ];
 
 @NgModule({
---
+++ MismeAdmin/src/app/pages/users/users-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationGuard } from 'src/app/core-mismes';
+import { UsersComponent } from './users.component';
+
+const routes: Routes = [
+  { path: '', component: UsersComponent, canActivate: [AuthenticationGuard] },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsersRoutingModule { }
