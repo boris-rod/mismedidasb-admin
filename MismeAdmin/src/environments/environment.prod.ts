82961da43d43c0d3b5058683ef23c4a83339e788
Yoandry <mazinchess@gmail.com>
2021-01-07 14:32:05 -0500

    Minor changes.

--- MismeAdmin/src/environments/environment.prod.ts
+++ MismeAdmin/src/environments/environment.prod.ts
@@ -5,6 +5,12 @@
  */
 export const environment = {
   production: true,
-  serverUrl: 'http://ec2-34-244-181-197.eu-west-1.compute.amazonaws.com/api/',
-  hubUrl: 'http://ec2-34-244-181-197.eu-west-1.compute.amazonaws.com/'
+  // serverUrl: 'http://ec2-34-244-181-197.eu-west-1.compute.amazonaws.com/api/',
+  // hubUrl: 'http://ec2-34-244-181-197.eu-west-1.compute.amazonaws.com/'
+  // serverUrl: 'http://ec2-34-244-181-197.eu-west-1.compute.amazonaws.com:8082/api/',
+  // hubUrl: 'http://ec2-34-244-181-197.eu-west-1.compute.amazonaws.com:8082/'
+  // serverUrl: 'https://api-dev.metriri.com/api/',
+  // hubUrl: 'https://api-dev.metriri.com/'
+  serverUrl: 'https://api.metriri.com/api/',
+  hubUrl: 'https://api.metriri.com/'
 };
