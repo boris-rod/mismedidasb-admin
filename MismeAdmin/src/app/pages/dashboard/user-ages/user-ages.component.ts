a9a3dd4476e106826f96a18805f55b73c93048bf
Yoandry <mazinchess@gmail.com>
2021-04-08 14:38:18 -0400

    Bug fix chart by ages rank.

--- MismeAdmin/src/app/pages/dashboard/user-ages/user-ages.component.ts
+++ MismeAdmin/src/app/pages/dashboard/user-ages/user-ages.component.ts
@@ -16,44 +16,43 @@ export class UserAgesComponent implements OnInit {
 
   ngOnInit(): void {
     this.userService.getUserByAgesSummary().subscribe(s => {
-      console.log(s.result);
       this.total = s.result.totalUsers;
       this.active = s.result.totalActiveUsers;
 
       const range1 = {
         name: '18 - 24', series: [
-          { name: 'Total', value: s.result.ageRange.countRange18To24 },
-          { name: '%', value: s.result.ageRange.percentageRange18To24 }
+          { name: 'Mujeres', value: s.result.ageRange.womenCountRange18To24 },
+          { name: 'Hombres', value: s.result.ageRange.menCountRange18To24 }
         ]
       };
       const range2 = {
         name: '25 - 34', series: [
-          { name: 'Total', value: s.result.ageRange.countRange25To34 },
-          { name: '%', value: s.result.ageRange.percentageRange25To34 }
+          { name: 'Mujeres', value: s.result.ageRange.womenCountRange25To34 },
+          { name: 'Hombres', value: s.result.ageRange.menCountRange25To34 }
         ]
       };
       const range3 = {
         name: '35 - 44', series: [
-          { name: 'Total', value: s.result.ageRange.countRange35To44 },
-          { name: '%', value: s.result.ageRange.percentageRange35To44 }
+          { name: 'Mujeres', value: s.result.ageRange.womenCountRange35To44 },
+          { name: 'Hombres', value: s.result.ageRange.menCountRange35To44 }
         ]
       };
       const range4 = {
         name: '45 - 54', series: [
-          { name: 'Total', value: s.result.ageRange.countRange45To54 },
-          { name: '%', value: s.result.ageRange.percentageRange45To54 }
+          { name: 'Mujeres', value: s.result.ageRange.womenCountRange45To54 },
+          { name: 'Hombres', value: s.result.ageRange.menCountRange45To54 }
         ]
       };
       const range5 = {
         name: '55 - 64', series: [
-          { name: 'Total', value: s.result.ageRange.countRange55To64 },
-          { name: '%', value: s.result.ageRange.percentageRange55To64 }
+          { name: 'Mujeres', value: s.result.ageRange.womenCountRange55To64 },
+          { name: 'Hombres', value: s.result.ageRange.menCountRange55To64 }
         ]
       };
       const range6 = {
         name: '65+', series: [
-          { name: 'Total', value: s.result.ageRange.countRangeMin65 },
-          { name: '%', value: s.result.ageRange.percentageRangeMin65 }
+          { name: 'Mujeres', value: s.result.ageRange.womenCountRangeMin65 },
+          { name: 'Hombres', value: s.result.ageRange.menCountRangeMin65 }
         ]
       };
 
